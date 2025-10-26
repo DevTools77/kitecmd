5824533dc4735376a2f5d9638fbe46a94219673e
coltonsr77 <coltonr3@icloud.com>
2025-10-26 07:49:00 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="kitecmd",
-    version="0.4",
+    version="0.5",
     author="coltonsr77",
-    description="A simple CLI example for PyPI",
-    packages=find_packages(),
+    description="A simple CLI example for PyPI that runs anywhere",
+    packages=find_packages(include=["kitecmd", "kitecmd.*"]),
     entry_points={
         "console_scripts": [
             "kitecmd=kitecmd.cli:main",
         ],
     },
+    include_package_data=True,
     python_requires=">=3.8",
 )
