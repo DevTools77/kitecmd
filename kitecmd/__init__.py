22a1c707d4902332ec400ffb962ca0cb8f5e3192
coltonsr77 <coltonr3@icloud.com>
2025-10-25 21:51:50 -0400

    Update __init__.py

--- kitecmd/__init__.py
+++ kitecmd/__init__.py
@@ -1,11 +1,1 @@
-"""
-KiteCMD
--------
-
-A simple command-line utility for demonstration and PyPI publishing.
-"""
-
-__version__ = "0.2"
-
-# Optional: Import the main entry function
-from .cli import main
+__version__ = "0.3"
